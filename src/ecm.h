a336a08237d16b51821277e10abb599997f1deb3
Daniel Carrasco <danielmadrid19@gmail.com>
2023-04-24 08:30:26 +0200

    Changed the license in one file and added it to the others

--- src/ecm.h
+++ src/ecm.h
@@ -3,17 +3,15 @@
  * Created by Daniel Carrasco at https://www.electrosoftcloud.com
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the Mozilla Public License 2.0.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Mozilla Public License 2.0 for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the Mozilla Public License 2.0
+ * along with this program.  If not, see <https://www.mozilla.org/en-US/MPL/2.0/>.
  *
  ******************************************************************************/
 
--- src/test_info_extract.cpp
+++ src/test_info_extract.cpp
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ *
+ * Created by Daniel Carrasco at https://www.electrosoftcloud.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Mozilla Public License 2.0.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Mozilla Public License 2.0 for more details.
+ *
+ * You should have received a copy of the Mozilla Public License 2.0
+ * along with this program.  If not, see <https://www.mozilla.org/en-US/MPL/2.0/>.
+ *
+ ******************************************************************************/
+
 #include "test.h"
 
 #define EXTRACT true
